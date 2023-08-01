9e574dbead929c87b3c8fbd2e087090589803ccf
thangnguyenw <nguyenhuuchienthangwww@gmail.com>
2023-08-01 12:43:50 +0700

    setup

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import express  from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
